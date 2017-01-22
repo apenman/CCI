548428cf0f3cf1e7263802f7c64b5de3e469f6e5
Alex <apenman427@gmail.com>
2017-01-21 16:02:27 -0800

    Made a script to create git repos :)

---
+++ git-create.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# check arguments. needs username and at least one repo name
+if [[ $# -eq 0 ]] ; then
+    echo 'Missing github username.'
+    exit 1
+fi
+
+if [[ $# -eq 1 ]] ; then
+    echo 'Missing at least one git repo name.'
+    exit 1
+fi
+
+user_name=$1
+# arg two should start repo names. need at least one
+# add check for this
+for i in ${@:2}
+do
+  repo_name=$i
+  curl -u $user_name https://api.github.com/user/repos -d "{\"name\":\"$repo_name\"}"
+  git clone https://github.com/$user_name/$repo_name.git
+  cd $repo_name
+  touch index.html
+  echo "<html>
+    <head>
+    </head>
+    <body>
+      Hi!
+    </body>
+  </html>" >> index.html
+  echo "Repo created with bash script :-)" >> README.md
+  git add .
+  git commit -m "Initialize repo with index.html"
+  git push origin master
+  git checkout -b gh-pages
+  git pull origin master
+  git push origin gh-pages
+  cd ..
+done

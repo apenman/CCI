f78031bf47a233b838a1dab7d558d8bde9091708
Alex <apenman427@gmail.com>
2017-02-01 22:58:37 -0800

    Adds final touches to emoji animation

--- week3/emoji-animation/sketch.js
+++ week3/emoji-animation/sketch.js
@@ -1,17 +1,23 @@
 var ghosts = [];
+var spooky;
+var numPieces = 10;
 
 function setup() {
+  spooky = loadSound('assets/boo.wav');
   createCanvas(windowWidth,windowWidth);
 }
 
 function draw() {
   background(255);
+  // Start from end of array so we can splice out with ease
   for(var i = ghosts.length-1; i >= 0; i--) {
-    console.log("Ghost at " + i + ": ", ghosts[i]);
     ghosts[i].move();
-    if(ghosts[i].fade <= 0) {
+    // if ghost life is below zero, kill it and remove from array
+    if(ghosts[i].life <= 0) {
+      ghosts[i].kill();
       ghosts.splice(i, 1);
     }
+    // else display
     else {
       ghosts[i].display();
     }
@@ -19,21 +25,68 @@ function draw() {
 }
 
 function mouseClicked() {
+  // Play spooky ghost sound
+  if(spooky.isPlaying()) {
+    spooky.stop();
+  }
+  spooky.play();
+  // Make 5 new ghosts at random points
   for(var i = 0; i < 5; i++) {
-    //console.log("Making new " + i);
     ghosts.push(new Ghost(random(windowWidth), random(windowHeight)));
   }
 }
 
 function Ghost(x, y) {
-  this.location = createVector(x, y);
-  this.fade = 50;
+  this.pieces = [];
+  this.life = 100;
+
+  // make body pieces
+  for(var i = 0; i < numPieces; i++) {
+    this.pieces.push(new GhostPiece(x, y, 10, 115, 115, 115));
+  }
+
+  // make eyes
+  // lil eye
+  this.pieces.push(new GhostPiece(x, y, 15, 0, 0, 0));
+  // big eye
+  this.pieces.push(new GhostPiece(x, y, 25, 0, 0, 0));
+
+  // make tongue
+  this.pieces.push(new GhostPiece(x, y, 10, 232, 39, 49));
+
+  this.move = function(){
+    this.life -= 1;
+    for(var i = 0; i < this.pieces.length; i++) {
+      this.pieces[i].move();
+    }
+  }
 
   this.display = function() {
-    ellipse(this.location.x, this.location.y, 10, 10);
+    for(var i = 0; i < this.pieces.length; i++) {
+      this.pieces[i].display();
+    }
   }
+
+  this.kill = function() {
+    this.pieces = [];
+  }
+}
+
+function GhostPiece(x, y, size, r, g, b) {
+  this.location = createVector(x, y);
+  this.accel = createVector(random(2), random(2));
+  this.fade = 100;
+
+  this.display = function() {
+    fill(r, g, b,this.fade);
+    noStroke();
+    ellipse(this.location.x, this.location.y, size);
+  }
+
   this.move = function() {
-    this.location.add(createVector(random(1), random(1)));
+    // Add random jitter to path
+    var accelWithJitter = this.accel.add(createVector(random(-1, 1)/4, random(-1, 1)/4));
+    this.location.add(accelWithJitter);
     this.fade -= 1;
   }
 }

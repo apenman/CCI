e17b86cdb2414187ed018c120498e321a064c87a
Alex <apenman427@gmail.com>
2017-02-02 19:00:15 -0800

    Adds mouse control

--- week3/circular/sketch.js
+++ week3/circular/sketch.js
@@ -1,23 +1,27 @@
 var angle = 0.05;
 var speed = 0.05;
-var scalar = 100;
+var xScale, yScale;
 var offsetX, offsetY;
 
 function setup() {
   offsetX = windowWidth / 2;
   offsetY = windowHeight / 2;
+  xScale = 200;
+  yScale = 50;
   createCanvas(windowWidth,windowWidth);
 }
 
 function draw() {
   background(255);
 
+  xScale = map(mouseX, 0, windowWidth, 50, 500);
+  yScale = map(mouseY, 0, windowHeight, 50, 500);
   // Draw path
   noFill();
-  ellipse(windowWidth / 2, windowHeight / 2, 200);
+  ellipse(windowWidth / 2, windowHeight / 2, xScale, yScale);
 
-  var x = offsetX + sin(angle) * scalar;
-  var y = offsetY + cos(angle) * scalar;
+  var x = offsetX + sin(angle) * (xScale/2);
+  var y = offsetY + cos(angle) * (yScale/2);
 
   // Draw orbiter
   fill(0);

0174f876c0a66f7de06840e44940df1d7a82611d
Alex <apenman427@gmail.com>
2017-02-28 20:58:49 -0800

    CONSOLE LOG NEW THINGS

--- week7/mybart/index.js
+++ week7/mybart/index.js
@@ -2,6 +2,7 @@ var bart = require('bart').createClient({"interval":1000});
 var count = 0;
 
 bart.on('powl', function(estimates){
-   console.log(estimates);
-   console.log(++count + " ______________________");
+  //  console.log(estimates);
+  //  console.log(++count + " ______________________");
+  console.log("Next train going to " + estimates[0].destination);
 });
